e7d8227ec459bce0ce6a26d67ff47691dcd3708a
Adam <adamjrate@hotmail.co.uk>
2017-08-08 14:07:59 +0000

    With unused Wait

--- DemoSiteTest/src/test/LoginPage.java
+++ DemoSiteTest/src/test/LoginPage.java
@@ -1,35 +1,45 @@
 package test;
 
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 
 public class LoginPage {
-	
-	@FindBy(xpath= "//input[contains(@name, 'username')]")
+
+
+
+	@FindBy(xpath = "//input[contains(@name, 'username')]")
 	private WebElement usernameInput;
-	
-	@FindBy (xpath = "//input[contains(@name, 'password')]")
+
+	@FindBy(xpath = "//input[contains(@name, 'password')]")
 	private WebElement passwordInput;
-	
-	@FindBy (xpath="//input[contains(@value, 'Test Login')]")
+
+	@FindBy(xpath = "//input[contains(@value, 'Test Login')]")
 	private WebElement submitButton;
 
-	@FindBy (xpath="/html/body/table/tbody/tr/td[1]/big/blockquote/blockquote/font/center/b")
+	@FindBy(xpath = "//blockquote/blockquote/font/center/b")
 	private WebElement successText;
-	
-	public void enterUsername(String username){
+
+	public void enterUsername(String username) {
 		usernameInput.sendKeys(username);
 	}
-	
-	public void enterPassword(String password){
+
+	public void enterPassword(String password) {
 		passwordInput.sendKeys(password);
 	}
-	
-	public void submitUserDetails(){
+
+	public void submitUserDetails() {
 		submitButton.click();
 	}
-	
-	public String getSuccessText(){
+
+	public String getSuccessText() {
 		return successText.getText();
 	}
 }
--- DemoSiteTest/src/test/WebTest.java
+++ DemoSiteTest/src/test/WebTest.java
@@ -2,23 +2,28 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+
 import org.junit.After;
 import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.junit.Before;
 import org.openqa.selenium.*;
 
 public class WebTest {
-	
+
 	private WebDriver webDriver;
 	private SignUpPage signUpPage;
 	private LoginPage loginPage;
 	private NavBar navBar;
-	
+
 	@Before
-	public void Before(){
+	public void Before() {
 		System.out.println("Before");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--start-maximized");
@@ -27,27 +32,41 @@ public class WebTest {
 		loginPage = PageFactory.initElements(webDriver, LoginPage.class);
 		navBar = PageFactory.initElements(webDriver, NavBar.class);
 	}
-	
+
+
 	@Test
-	public void Test(){
+	public void LoginTest() {
 		System.out.println("Test");
-		webDriver.navigate().to("http://www.TheDemoSite.co.uk");	
+		webDriver.navigate().to("http://www.TheDemoSite.co.uk");
 		navBar.goToAddUserPage();
 		signUpPage.enterUsername("Test");
 		signUpPage.enterPassword("Password");
-		signUpPage.saveUser();	
+		signUpPage.saveUser();
 		navBar.goToLoginPage();
 		loginPage.enterUsername("Test");
 		loginPage.enterPassword("Password");
 		loginPage.submitUserDetails();
-		assertEquals("**Successful Login**", loginPage.getSuccessText());				 //Check login was successful
-		
+		assertEquals("**Successful Login**", loginPage.getSuccessText());
+		Wait<WebDriver> wait = new FluentWait<WebDriver>(webDriver)
+				.withTimeout(10, TimeUnit.SECONDS)
+				.pollingEvery(5, TimeUnit.SECONDS)
+				.ignoring(NoSuchElementException.class);
+
+		WebElement foo = wait.until(new Function<WebDriver, WebElement>() {
+
+			public WebElement apply(WebDriver driver) {
+				return webDriver.findElement(By.xpath("//blockquote/blockquote/font/center/b"));
+
+			}
+		});
 	}
 	
+
 	@After
-	public void After(){
-		//webDriver.quit();
+	public void After() {
+
+		webDriver.quit();
 		System.out.println("After");
 	}
-	
+
 }
